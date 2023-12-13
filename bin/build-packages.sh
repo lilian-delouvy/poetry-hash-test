5d3384b35d9dda176dfae33e0232e293ffe5c92c
Lilian Delouvy <lilian.delouvy@axa.fr>
2023-12-13 13:37:48 +0100

    refactor(Packages): commit whl file to check incoherences

--- bin/build-packages.sh
+++ bin/build-packages.sh
@@ -3,12 +3,13 @@ cwd=$(pwd)
 cd my_local_package
 
 poetry install --sync
-poetry build --format wheel
+#find . -type f -print0 | xargs -0 dos2unix
+#poetry build --format wheel
 
 cd $cwd
 
-cp my_local_package/dist/*.whl consumer_package/packages
-echo "Python Package my_local_package successfully copied to consumer_package/packages"
+#cp my_local_package/dist/*.whl consumer_package/packages
+#echo "Python Package my_local_package successfully copied to consumer_package/packages"
 
 cd consumer_package
 
